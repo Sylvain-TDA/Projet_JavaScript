ba2f4f7044145d0b48bb3c1d436f63b58d02d86a
Sylvain-TDA <sylvain.teissier@le-campus-numerique.fr>
2025-04-30 09:52:39 +0200

    issue#15-dropdown JS

--- js/script.js
+++ js/script.js
@@ -45,7 +45,23 @@ for (i = 0; i < 10; i++) {
 }
 
 
+function dropDown () {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
+// Ferme le menu dropdown quand l'utilisateur clic ailleur
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
 
 
 
